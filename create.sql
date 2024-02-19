198a04d1f8d4d6846e8a18ed28f0e38eb168eed1
pedfav <pedfav@MacBook-Pro-de-Teste.local>
2024-02-18 21:46:57 -0300

    Aula 4

--- create.sql
+++ create.sql
@@ -1,8 +1,8 @@
-DROP SCHEMA IF EXISTS cccat15 cascade;
+drop schema if exists cccat15 cascade;
 
-CREATE SCHEMA cccat15;
+create schema cccat15;
 
-CREATE TABLE cccat15.account (
+create table cccat15.account (
 	account_id uuid primary key,
 	name text not null,
 	email text not null,
@@ -10,4 +10,28 @@ CREATE TABLE cccat15.account (
 	car_plate text null,
 	is_passenger boolean not null default false,
 	is_driver boolean not null default false
+);
+
+create table cccat15.ride (
+	ride_id uuid primary key,
+	passenger_id uuid,
+	driver_id uuid,
+	fare numeric,
+	distance numeric,
+	status text,
+	from_lat numeric,
+	from_long numeric,
+	to_lat numeric,
+	to_long numeric,
+	last_lat numeric,
+	last_long numeric,
+	date timestamp
+);
+
+create table cccat15.position (
+	position_id uuid primary key,
+	ride_id uuid,
+	lat numeric,
+	long numeric,
+	date timestamp
 );
